2756b73a7e33775f5f298665eafc9f927a3c0220
sobczi <38755429+sobczi@users.noreply.github.com>
2019-06-30 04:50:05 +0200

    Add titles for threads

--- GladiatusBOT/Program.cs
+++ GladiatusBOT/Program.cs
@@ -6,34 +6,28 @@ namespace GladiatusBOT
 {
     static class Program
     {
-        /// <summary>
-        /// Główny punkt wejścia dla aplikacji.
-        /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Thread t = new Thread(() =>
-            //{
-            //    Application.Run(new Form1());
-            //});
-            //t.SetApartmentState(ApartmentState.STA);
-            //t.Start();
-            //Thread form = new Thread();
-            //form.Start();
-
-
 
             Thread catch_mouse = new Thread(Sys.Catch_mouse);
+            catch_mouse.Name = "mouse";
             catch_mouse.IsBackground = true;
             catch_mouse.Start();
 
 
-            Bot.Run();
-            //Thread bot = new Thread(Bot.Run);
-            //bot.IsBackground = true;
-            //bot.Start();
+            Thread bot = new Thread(Bot.Run);
+            bot.Name = "bot";
+            bot.IsBackground = true;
+            bot.Start();
+
+            Bot.form = new Main();
+            Application.Run(Bot.form);
+
+            //Bot.form = new Settings();
+            //Application.Run(Bot.form);
         }
     }
 }
