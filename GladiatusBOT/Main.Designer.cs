da0db097caa1a3b9c5f005f814f7c9a0ed3ab370
sobczi <38755429+sobczi@users.noreply.github.com>
2019-06-30 04:49:13 +0200

    Created Main Form
    -> Update UI on mouse Move (sleep_mode)
    -> Force sleep_mode if sleep_btn clicked
    TODO: Deep debugging
    TODO: Connect functions to sell_items and take_gold buttons

---
+++ GladiatusBOT/Main.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace GladiatusBOT
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelProgress = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelLevel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelServer = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelRubles = new System.Windows.Forms.Label();
+            this.labelGold = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.labelDungeon = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelExpedition = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.sell_btn = new System.Windows.Forms.Button();
+            this.gold_btn = new System.Windows.Forms.Button();
+            this.sleep_btn = new System.Windows.Forms.Button();
+            this.settings_btn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelProgress);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.labelLevel);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.labelServer);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.labelName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(205, 128);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "User";
+            // 
+            // labelProgress
+            // 
+            this.labelProgress.AutoSize = true;
+            this.labelProgress.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelProgress.Location = new System.Drawing.Point(108, 100);
+            this.labelProgress.Name = "labelProgress";
+            this.labelProgress.Size = new System.Drawing.Size(86, 17);
+            this.labelProgress.TabIndex = 7;
+            this.labelProgress.Text = "undefined";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Progress";
+            // 
+            // labelLevel
+            // 
+            this.labelLevel.AutoSize = true;
+            this.labelLevel.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelLevel.Location = new System.Drawing.Point(108, 74);
+            this.labelLevel.Name = "labelLevel";
+            this.labelLevel.Size = new System.Drawing.Size(86, 17);
+            this.labelLevel.TabIndex = 5;
+            this.labelLevel.Text = "undefined";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Level";
+            // 
+            // labelServer
+            // 
+            this.labelServer.AutoSize = true;
+            this.labelServer.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelServer.Location = new System.Drawing.Point(108, 47);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Size = new System.Drawing.Size(86, 17);
+            this.labelServer.TabIndex = 3;
+            this.labelServer.Text = "undefined";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Server";
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelName.Location = new System.Drawing.Point(108, 19);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(86, 17);
+            this.labelName.TabIndex = 2;
+            this.labelName.Text = "undefined";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelRubles);
+            this.groupBox2.Controls.Add(this.labelGold);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.labelDungeon);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.labelExpedition);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Location = new System.Drawing.Point(223, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(203, 128);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Points";
+            // 
+            // labelRubles
+            // 
+            this.labelRubles.AutoSize = true;
+            this.labelRubles.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelRubles.Location = new System.Drawing.Point(105, 100);
+            this.labelRubles.Name = "labelRubles";
+            this.labelRubles.Size = new System.Drawing.Size(86, 17);
+            this.labelRubles.TabIndex = 9;
+            this.labelRubles.Text = "undefined";
+            // 
+            // labelGold
+            // 
+            this.labelGold.AutoSize = true;
+            this.labelGold.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelGold.Location = new System.Drawing.Point(105, 74);
+            this.labelGold.Name = "labelGold";
+            this.labelGold.Size = new System.Drawing.Size(86, 17);
+            this.labelGold.TabIndex = 9;
+            this.labelGold.Text = "undefined";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(3, 100);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(55, 17);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Rubles";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 17);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Gold";
+            // 
+            // labelDungeon
+            // 
+            this.labelDungeon.AutoSize = true;
+            this.labelDungeon.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelDungeon.Location = new System.Drawing.Point(105, 47);
+            this.labelDungeon.Name = "labelDungeon";
+            this.labelDungeon.Size = new System.Drawing.Size(86, 17);
+            this.labelDungeon.TabIndex = 9;
+            this.labelDungeon.Text = "undefined";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 47);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Dungeon";
+            // 
+            // labelExpedition
+            // 
+            this.labelExpedition.AutoSize = true;
+            this.labelExpedition.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelExpedition.Location = new System.Drawing.Point(105, 19);
+            this.labelExpedition.Name = "labelExpedition";
+            this.labelExpedition.Size = new System.Drawing.Size(86, 17);
+            this.labelExpedition.TabIndex = 4;
+            this.labelExpedition.Text = "undefined";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 19);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 17);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Expedition";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.sell_btn);
+            this.groupBox3.Controls.Add(this.gold_btn);
+            this.groupBox3.Controls.Add(this.sleep_btn);
+            this.groupBox3.Controls.Add(this.settings_btn);
+            this.groupBox3.Location = new System.Drawing.Point(12, 146);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(414, 95);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "author: sobczi";
+            // 
+            // sell_btn
+            // 
+            this.sell_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sell_btn.Location = new System.Drawing.Point(211, 55);
+            this.sell_btn.Name = "sell_btn";
+            this.sell_btn.Size = new System.Drawing.Size(196, 27);
+            this.sell_btn.TabIndex = 6;
+            this.sell_btn.Text = "Sell Items";
+            this.sell_btn.UseVisualStyleBackColor = true;
+            // 
+            // gold_btn
+            // 
+            this.gold_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.gold_btn.Location = new System.Drawing.Point(9, 55);
+            this.gold_btn.Name = "gold_btn";
+            this.gold_btn.Size = new System.Drawing.Size(196, 27);
+            this.gold_btn.TabIndex = 5;
+            this.gold_btn.Text = "Take Gold";
+            this.gold_btn.UseVisualStyleBackColor = true;
+            // 
+            // sleep_btn
+            // 
+            this.sleep_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sleep_btn.Location = new System.Drawing.Point(9, 22);
+            this.sleep_btn.Name = "sleep_btn";
+            this.sleep_btn.Size = new System.Drawing.Size(196, 27);
+            this.sleep_btn.TabIndex = 4;
+            this.sleep_btn.Text = "Sleep";
+            this.sleep_btn.UseVisualStyleBackColor = true;
+            this.sleep_btn.Click += new System.EventHandler(this.Sleep_btn_Click);
+            // 
+            // settings_btn
+            // 
+            this.settings_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.settings_btn.Location = new System.Drawing.Point(211, 22);
+            this.settings_btn.Name = "settings_btn";
+            this.settings_btn.Size = new System.Drawing.Size(196, 27);
+            this.settings_btn.TabIndex = 3;
+            this.settings_btn.Text = "Settings";
+            this.settings_btn.UseVisualStyleBackColor = true;
+            this.settings_btn.Click += new System.EventHandler(this.Settings_btn_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(432, 245);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Verdana", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Gladiatus Bot v0.1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelProgress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelLevel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelRubles;
+        private System.Windows.Forms.Label labelGold;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label labelDungeon;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelExpedition;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button sleep_btn;
+        private System.Windows.Forms.Button settings_btn;
+        private System.Windows.Forms.Button sell_btn;
+        private System.Windows.Forms.Button gold_btn;
+    }
+}
---
+++ GladiatusBOT/Main.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GladiatusBOT
+{
+    public partial class Main : Form
+    {
+        bool force_sleep = false;
+        public Main()
+        {
+            InitializeComponent();
+            Read_settings();
+            Thread w_sleep = new Thread(Watch_sleep);
+            w_sleep.IsBackground = true;
+            w_sleep.Start();
+        }
+        
+        void Watch_sleep()
+        {
+            bool last = Bot.sleep_mode;
+            while(!force_sleep)
+            {
+                if(last != Bot.sleep_mode)
+                {
+                    last = Bot.sleep_mode;
+                    FontStyle var = FontStyle.Regular;
+                    if (last)
+                        var = FontStyle.Bold;
+                    sleep_btn.Font = new System.Drawing.Font(sleep_btn.Font.Name, sleep_btn.Font.Size, var);
+                }
+                Thread.Sleep(Bot.wait);
+            }
+        }
+
+        void Read_settings()
+        {
+            labelName.Text = RegistryValues.Read_s("username");
+            labelServer.Text = Get.Read_server(RegistryValues.Read_i("server"));
+            FontStyle var = FontStyle.Regular;
+            if (RegistryValues.Read_b("c_sleep"))
+                var = FontStyle.Bold;
+            sleep_btn.Font = new System.Drawing.Font(sleep_btn.Font.Name, sleep_btn.Font.Size, var);
+        }
+
+        private void Settings_btn_Click(object sender, System.EventArgs e)
+        {
+            Settings s = new Settings();
+            s.Show();
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.Hide();
+            while (Bot.driver == null) { }
+            Bot.driver.Quit();
+            Application.Exit();
+        }
+
+        private void Sleep_btn_Click(object sender, System.EventArgs e)
+        {
+            force_sleep = true;
+            Bot.sleep_mode = true;
+            sleep_btn.Font = new System.Drawing.Font(sleep_btn.Font.Name, sleep_btn.Font.Size, FontStyle.Bold);
+        }
+    }
+}
