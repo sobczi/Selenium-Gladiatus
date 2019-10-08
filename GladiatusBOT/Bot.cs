4441dd4956748416234ecfe45c216b31273fbd41
sobczi <38755429+sobczi@users.noreply.github.com>
2019-10-08 05:07:53 +0200

    Update Bot.cs
    - Reads players name
    - Creates chrome when any action is set to ON

--- GladiatusBOT/Bot.cs
+++ GladiatusBOT/Bot.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.IO;
-using Microsoft.Win32;
 using System.Windows.Forms;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
-using System.Threading.Tasks;
 using System.Threading;
 using System.Drawing;
 
@@ -14,7 +12,6 @@ namespace GladiatusBOT
     {
         public static Main form;
         public static ChromeDriver driver;
-        public static bool logged = false;
         public static bool sleep_mode = RegistryValues.Read_b("c_sleep");
         public static bool work = true;
         static bool bool_sell_items = false;
@@ -26,6 +23,7 @@ namespace GladiatusBOT
             get { return bool_download_packages; }
             set { bool_download_packages = value; work = false; Set_regular_text(form.btnBotting); }
         }
+
         public static bool Sell_items
         {
             get { return bool_sell_items; }
@@ -37,13 +35,14 @@ namespace GladiatusBOT
             get { return bool_take_gold; }
             set { bool_take_gold = value; work = false; Set_regular_text(form.btnBotting); }
         }
+
         static public Actions ac;
         static public int wait = 250;
 
         private static bool Create_Chrome()
         {
             Thread.Sleep(2000);
-            while (!Bot.work) { Thread.Sleep(500); continue; }
+            while (!Bot.work && !Bot.Take_gold && !Bot.Sell_items) { Thread.Sleep(500); continue; }
             try
             {
                 var driverService = ChromeDriverService.CreateDefaultService();
@@ -53,12 +52,17 @@ namespace GladiatusBOT
                     driverOptions.AddArgument("--window-position=2000,0");
                 if (RegistryValues.Read_b("c_headless"))
                     driverOptions.AddArgument("--headless");
-                driverOptions.AddExtension("resources/GladiatusAddon.crx");
                 driver = new ChromeDriver(driverService, driverOptions);
                 driver.Manage().Window.Maximize();
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(wait);
                 ac = new Actions(driver);
-            }catch(Exception ex) { Basic.Kill_Chrome_Drivers(); Sys.Handle_exception(ex); return false; }
+            }
+            catch (Exception ex)
+            {
+                Basic.Kill_Chrome_Drivers();
+                Sys.Handle_exception(ex);
+                return false;
+            }
             return true;
         }
 
@@ -71,17 +75,15 @@ namespace GladiatusBOT
                 if (Create_Chrome())
                     try
                     {
-                        logged = Task.Login();
-                        if (!logged) { MessageBox.Show("Bot couldn't login into account..", "Error!"); work = false; break; }
+                        Task.Login();
+                        Update_name();
                         Update_ui();
-                        Task.Disable_notifications();
                         if (true)
                         {
                             while (true)
                             {
                                 if (work)
                                 {
-                                    Pack.Search();
                                     bool exp = true;
                                     bool dung = true;
                                     bool event_exists = true;
@@ -92,6 +94,8 @@ namespace GladiatusBOT
                                         dung = Task.Dungeon();
                                         if (event_exists)
                                             event_exists = Task.Event();
+                                        Task.Arena();
+                                        Task.Circus_Turma();
                                         Pack.Buy();
                                     }
                                     if (!work) continue;
@@ -126,15 +130,26 @@ namespace GladiatusBOT
                 return;
             }
         }
+            
+        static void Update_name()
+        {
+            Navigation.Main_menu("Podgląd");
+            string nickname = Get.Element("//div[contains(@class,'ellipsis')]").Text;
+            form.Invoke((MethodInvoker)delegate { form.labelName.Text = nickname; });
+        }
 
         static void Update_ui()
         {
-            form.Invoke((MethodInvoker)delegate { form.labelGold.Text = Convert.ToString(Get.Gold_s()); });
-            form.Invoke((MethodInvoker)delegate { form.labelRubles.Text = Convert.ToString(Get.Rubles()); });
-            form.Invoke((MethodInvoker)delegate { form.labelExpedition.Text = Convert.ToString(Get.Points_expedition()); });
-            form.Invoke((MethodInvoker)delegate { form.labelDungeon.Text = Convert.ToString(Get.Points_dungeon()); });
-            form.Invoke((MethodInvoker)delegate { form.labelLevel.Text = Convert.ToString(Get.Level()); });
-            form.Invoke((MethodInvoker)delegate { form.labelProgress.Text = Convert.ToString(Get.Progress()); });
+            try
+            {
+                form.Invoke((MethodInvoker)delegate { form.labelGold.Text = Convert.ToString(Get.Gold_s()); });
+                form.Invoke((MethodInvoker)delegate { form.labelRubles.Text = Convert.ToString(Get.Rubles()); });
+                form.Invoke((MethodInvoker)delegate { form.labelExpedition.Text = Convert.ToString(Get.Points_expedition()); });
+                form.Invoke((MethodInvoker)delegate { form.labelDungeon.Text = Convert.ToString(Get.Points_dungeon()); });
+                form.Invoke((MethodInvoker)delegate { form.labelLevel.Text = Convert.ToString(Get.Level()); });
+                form.Invoke((MethodInvoker)delegate { form.labelProgress.Text = Convert.ToString(Get.Progress()); });
+            }
+            catch { }
         }
 
         static void Set_regular_text(Button b)
